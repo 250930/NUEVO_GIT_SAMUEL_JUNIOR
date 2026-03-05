dec6a94de8c980bd37134433f3b7e8a3096b37ce
samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-04 20:38:36 -0500

    Ssegundo commit

--- usuario_B.py
+++ usuario_B.py
@@ -2,3 +2,7 @@ def primos_en_rango(inicio, fin):
     if inicio > fin:
         print("El valor inicial debe ser menor o igual al valor final")
         return
+    
+    for numero in range(inicio, fin + 1):
+        if numero < 2:
+            continue
