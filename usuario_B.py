221474f786464255bbe7064c1f1466746915e419
samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-04 20:49:43 -0500

    SegundoFuncion2

--- usuario_B.py
+++ usuario_B.py
@@ -19,3 +19,7 @@ def primos_en_rango(inicio, fin):
 def es_primo(numero):
     if numero < 2:
         return False
+    
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
