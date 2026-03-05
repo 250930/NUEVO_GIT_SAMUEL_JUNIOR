c8e4148334d20e2dfb0a83de03b3e2ae2c9edb06
samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-04 21:00:00 -0500

    commit tercera funcion

--- usuario_b.py
+++ usuario_b.py
@@ -29,3 +29,7 @@ def es_primo(numero):
 def factorial(n):
     if n < 0:
         return "No existe factorial para números negativos"
+    
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
