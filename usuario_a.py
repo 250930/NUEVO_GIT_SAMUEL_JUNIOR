6159389053fe596e1bc9927bad24c1c77731e97b
250930 <trabajozorro@gmail.com>
2026-03-04 21:08:41 -0500

    feat(usuario_a):creacion de la funcion de numero pefecto

--- usuario_a.py
+++ usuario_a.py
@@ -27,3 +27,15 @@ def capicua(numero):
     except ValueError:
         print("Error: debes ingresar un número válido")
         return False
+
+def numero_perfecto(numero):
+    suma = 0
+
+    for i in range(1, numero):
+        if numero % i == 0:
+            suma += i
+
+    if suma == numero:
+        return True
+    else:
+        return False
