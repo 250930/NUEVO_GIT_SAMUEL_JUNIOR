028ca2b271fd19e5b53f598443c7fd5ebddbf455
samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-04 20:41:35 -0500

    Final primera funcion

--- usuario_B.py
+++ usuario_B.py
@@ -6,3 +6,12 @@ def primos_en_rango(inicio, fin):
     for numero in range(inicio, fin + 1):
         if numero < 2:
             continue
+        
+        es_primo = True
+        for i in range(2, int(numero ** 0.5) + 1):
+            if numero % i == 0:
+                es_primo = False
+                break
+        
+        if es_primo:
+            print(numero, end=" ")
